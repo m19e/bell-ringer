d057a0bdb3070f49b39618bbd4f34d1f03638665
m19e <nexus.me7290@gmail.com>
2020-06-15 00:55:16 +0900

    Get mails following query

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,26 @@ function testGreeter() {
     const age = 12;
     Logger.log(greeter(age));
 }
+
+const main = () => {
+    const address = PropertiesService.getScriptProperties().getProperty("TARGET_MAIL_ADDRESS")
+    const query = `from:${address}`;
+    const threads = GmailApp.search(query, 0, 10);
+    const messagesForThreads = GmailApp.getMessagesForThreads(threads);
+
+    const values = [];
+    for (const messages of messagesForThreads) {
+        const message = messages[0];
+        const record = [
+            message.getDate(),
+            message.getFrom(),
+            message.getSubject(),
+            message.getPlainBody().slice(0, 200),
+        ];
+        values.push(record);
+    }
+
+    for (const value of values) {
+        Logger.log(value[2]);
+    }
+};
