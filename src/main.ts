0d291cd58658e2764b29b9ca3144e36130b25c01
m19e <nexus.me7290@gmail.com>
2020-06-16 18:08:16 +0900

    Add checkbox to judge is words in body

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ const main = () => {
             message.getSubject(),
             // message.getPlainBody(),
             "TEXT",
+            checkWordInBody(message.getPlainBody(), words),
         ];
         values.push(record);
     }
